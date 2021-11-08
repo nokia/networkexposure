c32280565320c8d32aaf9aaeb8a6099e660a2ffa
Som-Som-CC <84196538+Som-Som-CC@users.noreply.github.com>
2021-11-08 20:05:53 +0100

    notif-wake and notif-callDirection examples

---
+++ README.md
@@ -0,0 +1,18 @@
+# Network Exposure
+
+## Overview
+
+Network exposure is a technology to let app developers communicate with their devices or control various aspects of a network.
+Targeting IoT, Industry 4.0, multi-media streaming, cloud gaming, smart agriculture, smart vehicles, public safety, and also good old telephony.
+
+For example you can subscribe to the event when your battery operated, power restrained device wakes up and gets ready for data communication.
+Or you may set bandwidth (QoS) for certain communication, and may pay the related fees.
+Or check calling numbers and block spammers for millions of subscribers instead of managing blocking list on each device one-by-one.
+
+You can combine these API calls with other non-telecom services (mail, calendar, banking, toaster) to come up with your unique app.
+
+## Code snippets
+
+This repository contains a couple of basic code snippets, which may let you get started more easily.
+You may just read standards, like 3GPP TS 29.122 and 29.522. But it is not easy to see what these offer and how to use them.
+The authors of these examples hope you find them helpful. Have fun.
---
+++ examples/notif-wake/sh/notif-wake.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 Nokia
+# Licensed under the BSD 3-Clause Clear License.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+# Get notification when device is available for data transmission.
+
+set -e
+
+function print_help() {
+    echo Usage: $0 '-n NEF_URL -u USERNAME -p PASSWORD -a AF_ID -N NOTIFY_URL'
+}
+
+while getopts m:n:u:p:a:N:h opts; do
+   case ${opts} in
+      m) MSISDN=$OPTARG ;;
+      n) NEF=$OPTARG ;;
+      u) USERNAME=$OPTARG ;;
+      p) PASSWORD=$OPTARG ;;
+      a) AF=$OPTARG ;;
+      N) NOTIFYURL=$OPTARG ;;
+      h) print_help ;;
+      ?) print_help ;;
+   esac
+done
+
+if [ -z "$NEF" ] || [ -z "$USERNAME" ] || [ -z "$PASSWORD" ] || [ -z "$AF" ] || [ -z "$NOTIFYURL" ]; then print_help; fi
+
+# Subscribe
+RESOURCE=$(curl $NEF/3gpp-monitoring-event/v1/$AF/subscriptions \
+    --user $USERNAME:$PASSWORD \
+    -i -sSf -b cookies.txt -c cookies.txt \
+    -H 'Content-Type: application/json' \
+    -d '{"msisdn": "'$MSISDN'", "notificationDestination": "'$NOTIFYURL'", "monitoringType": "UE_REACHABILITY", "reachabilityType": "DATA", "maximumNumberOfReports": 1}' \
+    | grep -Ei '^Location:' | sed -r 's/^Location: *//i' | tr -d '\r\n')
+
+if [ -z "$RESOURCE" ]; then
+    echo Error: Subscription failed.
+    false
+fi
+
+echo Resource URL: $RESOURCE
+
+# Query subscription
+echo Queried:
+curl $RESOURCE --fail -sSf --user $USERNAME:$PASSWORD -b cookies.txt -c cookies.txt | jq
+
+# Receive notification
+# Make sure NOTIFYURL is in line with this.
+echo "HTTP/1.1 200 OK" | nc -l -p 8080
+
+# Delete subscription
+curl $RESOURCE -sSf --user $USERNAME:$PASSWORD -b cookies.txt -c cookies.txt -X DELETE
+
+# If you experimented a lot and want to get rid of all subscriptions, you may try this:
+# curl --user $USERNAME:$PASSWORD -c cookies.txt -b cookies.txt $NEF/3gpp-monitoring-event/v1/$AF/subscriptions | jq '.[].self' | xargs curl --user $USERNAME:$PASSWORD -b cookies.txt -X DELETE
